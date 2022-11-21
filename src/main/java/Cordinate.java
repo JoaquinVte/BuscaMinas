400a1e2fb228e9b4571cc686c3bee74ee5249c1b
Joaquin Vte. Alonso Saiz <jv.alonso@edu.gva.es>
2022-11-21 16:46:14 +0100

    a

---
+++ src/main/java/Cordinate.java
@@ -0,0 +1,46 @@
+public class Cordinate {
+    public static String getRandomCordinate(){
+        return String.valueOf((char)Input.getRandomInteger('J','A'))+String.valueOf((char)Input.getRandomInteger('9','0'));
+    }
+    public static boolean isCordinateValid(String cordinate){
+        if(cordinate.length()!=2) return false;
+
+        if(cordinate.charAt(0)>'J' || cordinate.charAt(0)<'A')
+            return false;
+        if(cordinate.charAt(1)>'9' || cordinate.charAt(1)<'0')
+            return false;
+
+        return true;
+    }
+    public static int getRowFromCoordinate(String cordinate){
+        return cordinate.charAt(0)-'A';
+    }
+    public static int getColumnFromCoordinate(String cordinate){
+        return cordinate.charAt(1)-'0';
+    }
+    public static String getUp(String cordinate){
+        return String.valueOf((char)(cordinate.charAt(0)-1)) + cordinate.charAt(1);
+    }
+    public static String getDown(String cordinate){
+        return String.valueOf((char)(cordinate.charAt(0)+1)) + cordinate.charAt(1);
+    }
+    public static String getRight(String cordinate){
+        return String.valueOf((char)(cordinate.charAt(0))) + (char)(cordinate.charAt(1)+1);
+    }
+    public static String getLeft(String cordinate){
+        return String.valueOf((char)(cordinate.charAt(0))) + (char)(cordinate.charAt(1)-1);
+    }
+
+    public static String getUpRight(String cordinate){
+        return Cordinate.getUp(Cordinate.getRight(cordinate));
+    }
+    public static String getUpLeft(String cordinate){
+        return Cordinate.getUp(Cordinate.getLeft(cordinate));
+    }
+    public static String getDownRight(String cordinate){
+        return Cordinate.getDown(Cordinate.getRight(cordinate));
+    }
+    public static String getDownLeft(String cordinate){
+        return Cordinate.getDown(Cordinate.getLeft(cordinate));
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,66 @@
 public class Main {
+    public static final char bomb = '\u2622';
 
     public static void main(String[] args) {
 
-        char[][] tablero = new char[10][10];
 
-        for(int i=0;i<100;i++)
-        System.out.println(Input.getRandomCordinate());
+
+        char[][] tablero = new char[10][10];
+        int bombs = 10;
+
+        fill(tablero);
+        placeBombs(bombs,tablero);
+        Screen.showDebug(tablero);
 
     }
 
     public static void fill(char[][] board){
         for(int fil=0;fil< board.length;fil++ )
             for(int col=0;col<board[fil].length;col++)
-                board[fil][col]=0;
+                board[fil][col]='0';
     }
+
+    public static void placeBombs(int bombs, char[][] board){
+        String c;
+        for(int i=0;i<bombs;i++){
+            do {
+                c = Cordinate.getRandomCordinate();
+            } while (board[Cordinate.getRowFromCoordinate(c)][Cordinate.getColumnFromCoordinate(c)] == bomb );
+            placeBomb(board,c);
+        }
+    }
+    public static void placeBomb(char[][] board, String c){
+        board[Cordinate.getRowFromCoordinate(c)][Cordinate.getColumnFromCoordinate(c)] = Main.bomb;
+
+        if(Cordinate.isCordinateValid(Cordinate.getUp(c)))
+            incrementar(board,Cordinate.getUp(c));
+        if(Cordinate.isCordinateValid(Cordinate.getDown(c)))
+            incrementar(board,Cordinate.getDown(c));
+        if(Cordinate.isCordinateValid(Cordinate.getRight(c)))
+            incrementar(board,Cordinate.getRight(c));
+        if(Cordinate.isCordinateValid(Cordinate.getLeft(c)))
+            incrementar(board,Cordinate.getLeft(c));
+
+        if(Cordinate.isCordinateValid(Cordinate.getUpRight(c)))
+            incrementar(board,Cordinate.getUpRight(c));
+        if(Cordinate.isCordinateValid(Cordinate.getUpLeft(c)))
+            incrementar(board,Cordinate.getUpLeft(c));
+        if(Cordinate.isCordinateValid(Cordinate.getDownRight(c)))
+            incrementar(board,Cordinate.getDownRight(c));
+        if(Cordinate.isCordinateValid(Cordinate.getDownLeft(c)))
+            incrementar(board,Cordinate.getDownLeft(c));
+
+    }
+    public static void incrementar(char[][] board, String c){
+        int fil = Cordinate.getRowFromCoordinate(c);
+        int col = Cordinate.getColumnFromCoordinate(c);
+
+        if(board[fil][col]!=Main.bomb){
+            int value = board[fil][col] - '0';
+            board[fil][col] = String.valueOf(value+1).charAt(0);
+        }
+    }
+
 
 
 }
